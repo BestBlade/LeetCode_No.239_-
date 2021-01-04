c197e8118b537b6e001c392901c030e6c0af10a5
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-01-04 18:45:50 +0800

    Add files via upload

---
+++ leetcode_no239_滑动窗口最大值_w2.cpp
@@ -0,0 +1,102 @@
+﻿/* ------------------------------------------------------------------|
+给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+
+返回滑动窗口中的最大值。
+
+ 
+
+示例 1：
+
+输入：nums = [1,3,-1,-3,5,3,6,7], k = 3
+输出：[3,3,5,5,6,7]
+解释：
+滑动窗口的位置                最大值
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+示例 2：
+
+输入：nums = [1], k = 1
+输出：[1]
+示例 3：
+
+输入：nums = [1,-1], k = 1
+输出：[1,-1]
+示例 4：
+
+输入：nums = [9,11], k = 2
+输出：[11]
+示例 5：
+
+输入：nums = [4,-2], k = 2
+输出：[4]
+ 
+
+提示：
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+1 <= k <= nums.length
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sliding-window-maximum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	双指针法
+*
+*	执行用时：496 ms, 在所有 C++ 提交中击败了61.25%的用户
+*	内存消耗：116.4 MB, 在所有 C++ 提交中击败了38.43%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    deque<int> q;
+    int len = nums.size();
+    for (int i = 0; i < k; i++) {
+        while (!q.empty() && nums[i] >= nums[q.back()]) {
+            q.pop_back();
+        }
+        q.emplace_back(i);
+    }
+    vector<int> res = { nums[q.front()] };
+    for (int i = k; i < len; i++) {
+        while (!q.empty() && nums[i] >= nums[q.back()]) {
+            q.pop_back();
+        }
+        q.emplace_back(i);
+        while (q.front() <= i - k) {
+            q.pop_front();
+        }
+        res.emplace_back(nums[q.front()]);
+    }
+    return res;
+}
